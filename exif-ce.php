bc2225773c22942d4e1b19907830ee340b00bed5
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2020-11-23 22:42:35 +0900

    add plugin infomation

---
+++ exif-ce.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name: Exif CE: Cechk & Erase
+ * Plugin URI:  https://www.braveryk7.com/
+ * Description: A plugin that allows you to check, delete, and manage Exif information for images.
+ * Version:     0.0.1
+ * Author:      Ken-chan
+ * Author URI:  https://twitter.com/braveryk7
+ * Text Domain: exif-ce
+ * Domain Path: /languages
+ * License:     GPL2
+ *
+ * @author Ken-chan
+ * @package WordPress
+ * @subpackage Exif CE
+ * @since 0.0.1
+ */
