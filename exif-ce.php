f5c919eeb7ddd57fed2fb48c9ca5f79c964df53a
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2020-11-23 23:20:12 +0900

    change require php version

--- exif-ce.php
+++ exif-ce.php
@@ -24,7 +24,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 require_once __DIR__ . '/class/class-ece-judgment-php-version.php';
 
-$require_php_version  = '7.5.0';
+$require_php_version  = '7.3.0';
 $get_php_version_bool = new ECE_Judgment_Php_Version();
 if ( false === $get_php_version_bool->judgment( $require_php_version ) ) {
 	require_once ABSPATH . 'wp-admin/includes/plugin.php';
