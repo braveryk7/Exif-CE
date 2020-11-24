caf455a7e4cd270fd995557e8705211b24d61c3e
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2020-11-24 11:51:10 +0900

    add menu hook

--- class/class-exif-ce-main.php
+++ class/class-exif-ce-main.php
@@ -18,4 +18,23 @@ if ( ! defined( 'ABSPATH' ) ) {
  * Exif CE main class.
  */
 class Exif_Ce_Main {
+	/**
+	 * WordPress Hook.
+	 */
+	public function __construct() {
+		add_action( 'admin_menu', [ $this, 'add_ece_menu' ] );
+	}
+
+	/**
+	 * Insert Admin menu > Tools.
+	 */
+	public function add_ece_menu() {
+		add_management_page(
+			__( 'Exif CE', 'exif-ce' ),
+			__( 'Exif CE', 'exif-ce' ),
+			'manage_options',
+			'exif-ce',
+			[ 'Ece_Admin_Page', 'admin_page' ],
+		);
+	}
 }
