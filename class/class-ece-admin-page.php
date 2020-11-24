7a90d32e7939ada61d35c5d9cf3822f67bff36e2
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2020-11-24 11:53:52 +0900

    add default

---
+++ class/class-ece-admin-page.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Exif CE admin page class.
+ *
+ * @author Ken-chan
+ * @package WordPress
+ * @subpackage Exif CE
+ * @since 0.0.1
+ */
+
+declare( strict_types = 1 );
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit( 'You do not have access rights.' );
+}
+
+/**
+ * Admin page class.
+ */
+class Ece_Admin_Page {
+}
